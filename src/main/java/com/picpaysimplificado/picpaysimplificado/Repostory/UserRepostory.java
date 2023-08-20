a0b62eee035e9355df3b2a381ce3c3421176a108
MuriloColleti <mu_colleti@outlook.com>
2023-08-20 16:44:43 -0300

    feat: Create TransactionRepository.

---
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/Repostory/UserRepostory.java
@@ -0,0 +1,11 @@
+package com.picpaysimplificado.picpaysimplificado.Repostory;
+
+import org.apache.catalina.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepostory extends JpaRepository<User , Long> {
+    Optional<User> findByDocument(String Document);
+
+}
