2c7cbb370df8fd7aaa8a00bab52e18a574e4488d
MuriloColleti <mu_colleti@outlook.com>
2023-09-12 21:41:36 -0300

    feat: update

--- src/main/java/com/picpaysimplificado/Controllers/UserController.java
+++ src/main/java/com/picpaysimplificado/Controllers/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +21,7 @@ public class UserController {
     private UserService userService;
 
     @PostMapping
-    public User createUser(UserDTO user){
+    public User createUser(@RequestBody UserDTO user){
         User newUser = userService.createUser(user);
         return new ResponseEntity<>(newUser, HttpStatus.CREATED).getBody();
     }
