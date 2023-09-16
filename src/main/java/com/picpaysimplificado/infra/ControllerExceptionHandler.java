075781bdb7977f48c719959d7c063ddaa8d3bec7
MuriloColleti <mu_colleti@outlook.com>
2023-09-16 15:14:13 -0300

    feat: Create threat exception!!

---
+++ src/main/java/com/picpaysimplificado/infra/ControllerExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.picpaysimplificado.infra;
+
+import com.picpaysimplificado.dtos.ExceptionDTO;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity threatDuplicationEntry(DataIntegrityViolationException exception){
+        ExceptionDTO exceptionDTO = new ExceptionDTO("Usario ja cadastrado", "400");
+        return ResponseEntity.badRequest().body(exceptionDTO);
+    }
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity threat484(EntityNotFoundException exception){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity threatGeneralExcption (Exception exception){
+        ExceptionDTO exceptionDTO = new ExceptionDTO(exception.getMessage(), "500");
+        return ResponseEntity.internalServerError().body(exceptionDTO);
+    }
+}
